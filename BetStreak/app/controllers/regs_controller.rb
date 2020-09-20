a8386072431efa0289cebde6b670bc26e8a340ca
Mark A Guza <guzamaguza@gmail.com>
2020-09-20 06:28:49 +0000

    upd

---
+++ BetStreak/app/controllers/regs_controller.rb
@@ -0,0 +1,2 @@
+class RegsController < ApplicationController
+end
--- BetStreak/config/routes.rb
+++ BetStreak/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :answers
+  resources :questions
+  resources :regs
+  resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "users#new"
 
