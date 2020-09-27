24dfed27755e99aa2d14bfcb13bf380b8c826828
Mark A Guza <guzamaguza@gmail.com>
2020-09-27 20:03:10 +0000

    update

--- BetStreak/db/seeds.rb
+++ BetStreak/db/seeds.rb
@@ -7,103 +7,102 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-  #Game.delete_all
-  #Question.delete_all
+  Game.delete_all
+  Question.delete_all
 
-  @games_data_1 = [{ title: 'MLB: Atlanta Braves V. Philadelphia Phillies' }, { datetime_of_start: '2021-08-30 16:00:00 UTC' }, { datetime_of_end: '' }, { location: 'Philadelphia, Pennsylvania' }, { timezone: 'EST' },{ match_status: 'Upcoming' }]
-
-  Game.create!(@games_data_1)
+  @game_data_1 = [ title: 'MLB: Atlanta Braves V. Philadelphia Phillies', datetime_of_start: '2021-08-30 16:00:00 UTC',  datetime_of_end: '' ,  location: 'Philadelphia, Pennsylvania' ,  timezone: 'EST' , match_status: 'Upcoming' ]
+  @game_1 = Game.create!(@game_data_1)
 
   @questions_data_1 = [
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '1' },
-      { content: 'Will either team score this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '1',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '2' },
-      { content: 'Will there be a home run this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '2',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '3' },
-      { content: 'Which pitcher will have more strikeouts this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '3',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '4' },
-      { content: 'How many extra base hits (2b, 3b, hr) will occur this inning? Over/Under 1.5' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '4',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '5' },
-      { content: 'Will there be a pitching change this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '5',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '6' },
-      { content: 'Will there be a lead change this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '6',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '7' },
-      { content: 'Will a double play occur this inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '7',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '8' },
-      { content: 'How many walks will be issued this inning? Over/Under 1.5' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '8',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ],
 
-    [ { game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id },
-      { q_num: '9' },
-      { content: 'Will either side of this inning be a 1-2-3 inning? Yes/No' },
-      { answer: '' },
-      { datetime_required: '' },
-      { datetime_submitted: '' },
-      { accepted: '' },
-      { result: '' }
+    [ game_id: Game.find_by(title: "MLB: Atlanta Braves V. Philadelphia Phillies").id,
+      q_num: '9',
+      content: 'Will either team score this inning? Yes/No',
+      answer: '',
+      datetime_required: '',
+      datetime_submitted: '',
+      accepted: '',
+      result: ''
     ]
 
   ]
@@ -112,13 +111,14 @@
     Question.create!(ques)
   end
 
-
+=begin
 def main
   #make_games
   make_questions
   #make_admin
   #make_attractions_and_rides
 end
+
 
 def make_games
   Game.create!(@games_data_1)
@@ -131,3 +131,4 @@ def make_questions
 end
 
 main
+=end
