44c465201964eee6ad9d769bcf90beb047396a92
Mark A Guza <guzamaguza@gmail.com>
2020-09-29 06:22:27 +0000

    updated

--- BetStreak/app/controllers/application_controller.rb
+++ BetStreak/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :redirect_if_not_logged_in
   helper_method :current_user, :logged_in?
 
-  private
+  #private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
--- BetStreak/app/controllers/regs_controller.rb
+++ BetStreak/app/controllers/regs_controller.rb
@@ -1,2 +1,22 @@
 class RegsController < ApplicationController
+
+  def new
+    @reg = Reg.new
+  end
+
+  def create
+     @reg = current_user.regs.build(reg_params)
+     if @reg.save
+      redirect_to reg_path(@reg)
+     else
+      render :new
+     end
+  end
+
+  private
+
+  def reg_params
+     params.require(:reg).permit()
+  end
+
 end
