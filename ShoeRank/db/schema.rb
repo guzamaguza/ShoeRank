b89f3c7a6021f3e348bbd66bc13a3e3c1b3f94de
Mark A Guza <guzamaguza@gmail.com>
2020-11-28 22:52:00 +0000

    updatedd

--- ShoeRank/db/schema.rb
+++ ShoeRank/db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_09_20_062802) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
-    t.string "class"
+    t.string "luxury_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
